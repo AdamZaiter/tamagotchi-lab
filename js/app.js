c56caf4a017e05986fd908040423891cae0de80f
Gellokd <adamzaiter23@gmail.com>
2024-06-14 05:52:37 -0500

    message describing code changes

--- js/app.js
+++ js/app.js
@@ -3,8 +3,15 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
+// Define the variables used the stack the state of the game.
+let state = {
+    boredom: 0, // Initializes boredom to 0
+    hunger: 0, // Initalizes hunger to 0
+    sleepiness: 0 // Initalizes sleepiness to 0
+}; 
 
-
+let timer; // Timer variable
+let gameOver; // gameOver variable to represent if player lost the game.
 
 /*------------------------ Cached Element References ------------------------*/
 
